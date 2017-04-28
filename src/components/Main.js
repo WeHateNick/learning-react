08982fa98553313d8c5669d80e785319a41e6dfc
Nick Ocampo <nick@wehatenick.com>
2017-04-27 23:47:20 -0700

    refactored header and players into individual components

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,61 @@ require('styles/App.css');
 import React from 'react';
 import PropTypes from 'prop-types';
 
+class Header extends React.Component {
+	
+	constructor(props) {
+    super(props);
+  }
+
+	render() {
+		return (
+			<div className="header">
+				<h1>{this.props.title}</h1>
+			</div>
+		)
+	}
+}
+Header.propTypes = {
+	title: PropTypes.string.isRequired
+}
+
+Header.defaultProps = {
+	title: 'Scoreboard'
+};
+
+class Player extends React.Component {
+
+	constructor(props) {
+    super(props);
+  }
+
+	render() {
+		return (
+			<div className="player">
+    		<div className="player-name">
+    			{this.props.name}
+    		</div>
+    		<div className="player-score">
+    			<div className="counter">
+    				<button className="counter-action decrement"> - </button>
+    				<div className="counter-score"> {this.props.score} </div>
+    				<button className="counter-action increment"> + </button>
+    			</div>
+    		</div>
+    	</div>
+		)
+	}
+}
+Player.propTypes = {
+	name: PropTypes.string.isRequired,
+	score: PropTypes.number.isRequired
+}
+
+Player.defaultProps = {
+	// name: 'Player 1',
+	score: 0
+};
+
 class AppComponent extends React.Component {
 	
 	constructor(props) {
@@ -13,51 +68,20 @@ class AppComponent extends React.Component {
   render() {
     return (
     	<div className="scoreboard">
-      	<div className="header">
-      		<h1>{this.props.title}</h1>
-      	</div>
-
+    		<Header title="Nick's Scoreboard" />
 		    <div className="players">
-		    	<div className="player">
-		    		<div className="player-name">
-		    			Nick Ocampo
-		    		</div>
-		    		<div className="player-score">
-		    			<div className="counter">
-		    				<button className="counter-action decrement"> - </button>
-		    				<div className="counter-score"> 31 </div>
-		    				<button className="counter-action increment"> + </button>
-		    			</div>
-		    		</div>
-		    	</div>
+		    	<Player name="Nick Ocampo" score="31" />
+		    	<Player name="Alex Montague" score="28" />
 		    </div>
-
-		    <div className="players">
-		    	<div className="player">
-		    		<div className="player-name">
-		    			Alex Montague
-		    		</div>
-		    		<div className="player-score">
-		    			<div className="counter">
-		    				<button className="counter-action decrement"> - </button>
-		    				<div className="counter-score"> 31 </div>
-		    				<button className="counter-action increment"> + </button>
-		    			</div>
-		    		</div>
-		    	</div>
-		    </div>
-
 		  </div>
     );
   }
 }
 
 AppComponent.propTypes = {
-	title: PropTypes.string.isRequired
 }
 
 AppComponent.defaultProps = {
-	title: 'Scoreboard'
 };
 
 export default AppComponent;
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './components/Main';
 
 // Render the main component into the dom
-ReactDOM.render(<App title="Nick's Scoreboard" />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
